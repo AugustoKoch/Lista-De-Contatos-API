9c1387ab2b85fb155377499cbf2770d347cc032f
Augusto Koch <augustok872@gmail.com>
2024-07-18 17:47:56 -0300

    Implementação do metodo updateContato

--- src/main/java/com/augusto/ListaDeContatos/controllers/ContatoController.java
+++ src/main/java/com/augusto/ListaDeContatos/controllers/ContatoController.java
@@ -3,13 +3,14 @@ package com.augusto.ListaDeContatos.controllers;
 import com.augusto.ListaDeContatos.models.Contato;
 import com.augusto.ListaDeContatos.models.Endereco;
 import com.augusto.ListaDeContatos.repositories.ContatoRepository;
-import com.augusto.ListaDeContatos.repositories.EnderecoRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/contato")
@@ -30,7 +31,7 @@ public class ContatoController {
     }
 
     @PostMapping
-    public ResponseEntity<Contato> CreateContato(@RequestBody Contato contato) {
+    public ResponseEntity<Contato> createContato(@RequestBody Contato contato) {
 
         for (Endereco endereco : contato.getEnderecos()) {
             endereco.setContato(contato);
@@ -38,6 +39,27 @@ public class ContatoController {
 
         contatoRepository.save(contato);
         return new ResponseEntity<>(contato, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Contato> updateContato(@PathVariable Long id, @RequestBody Contato contato) {
+        Optional<Contato> contatoOptional = contatoRepository.findById(id);
+
+        if (contatoOptional.isPresent()) {
+            Contato contatoExistente = contatoOptional.get();
+            BeanUtils.copyProperties(contato, contatoExistente, "id", "enderecos");
+
+            contatoExistente.getEnderecos().clear();
+            for (Endereco endereco : contato.getEnderecos()) {
+                endereco.setContato(contatoExistente);
+                contatoExistente.getEnderecos().add(endereco);
+            }
+
+            contatoRepository.save(contatoExistente);
+            return new ResponseEntity<>(contatoExistente, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
