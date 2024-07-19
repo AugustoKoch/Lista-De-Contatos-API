15f423c83da7f42509b25f19dd6f60be6cf96a89
Augusto Koch <augustok872@gmail.com>
2024-07-19 17:09:14 -0300

    Documentação com Swagger

--- src/main/java/com/augusto/ListaDeContatos/ListaDeContatosApplication.java
+++ src/main/java/com/augusto/ListaDeContatos/ListaDeContatosApplication.java
@@ -1,8 +1,12 @@
 package com.augusto.ListaDeContatos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Lista de Contatos",
+		version = "1.0", description = "Documentação para API de lista de contatos"))
 @SpringBootApplication
 public class ListaDeContatosApplication {
 
--- src/main/java/com/augusto/ListaDeContatos/controllers/ContatoController.java
+++ src/main/java/com/augusto/ListaDeContatos/controllers/ContatoController.java
@@ -3,6 +3,10 @@ package com.augusto.ListaDeContatos.controllers;
 import com.augusto.ListaDeContatos.models.Contato;
 import com.augusto.ListaDeContatos.models.Endereco;
 import com.augusto.ListaDeContatos.repositories.ContatoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,11 +20,17 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/contato")
+@Tag(name = "Lista de Contatos")
 public class ContatoController {
 
     @Autowired
     private ContatoRepository contatoRepository;
 
+    @Operation(summary = "Busca todos os contatos na base de dados", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Contatos encontrados com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Nenhum contato encontrado")
+    })
     @GetMapping
     public ResponseEntity<List<Contato>> getAllContatos() {
         List<Contato> contatos = contatoRepository.findAll();
@@ -32,6 +42,10 @@ public class ContatoController {
         return new ResponseEntity<>(contatos, HttpStatus.OK);
     }
 
+    @Operation(summary = "Cadastra um novo contato", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Contato cadastrado com sucesso")
+    })
     @PostMapping
     public ResponseEntity<Contato> createContato(@RequestBody Contato contato) {
 
@@ -43,6 +57,11 @@ public class ContatoController {
         return new ResponseEntity<>(contato, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Atualiza todos os dados de um contato", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Contato não encontrado")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Contato> updateContato(@PathVariable Long id, @RequestBody Contato contato) {
         Optional<Contato> contatoOptional = contatoRepository.findById(id);
@@ -64,6 +83,11 @@ public class ContatoController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Atualiza somente os dados especificados de um contato", method = "PATCH")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Contato não encontrado")
+    })
     @PatchMapping("/{id}")
     public ResponseEntity<Contato> updateItemsContato(@PathVariable Long id, @RequestBody Map<String, Object> updates) {
         Optional<Contato> contatoOptional = contatoRepository.findById(id);
@@ -105,10 +129,15 @@ public class ContatoController {
             contatoRepository.save(contatoExistente);
             return new ResponseEntity<>(contatoExistente, HttpStatus.OK);
         } else {
-            throw new RuntimeException("Contato não encontrado");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
+    @Operation(summary = "Deleta um contato", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Contato deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Contato não encontrado")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Contato> deleteContato(@PathVariable Long id) {
         Optional<Contato> contatoOptional = contatoRepository.findById(id);
@@ -117,7 +146,7 @@ public class ContatoController {
             contatoRepository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            throw new RuntimeException("Contato não encontrado");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
